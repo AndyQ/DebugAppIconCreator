999cd551530793f6a6055ed980f3e2b1e2e9b354
AndyQ <andy.qua@gmail.com>
2014-03-02 15:47:18 +0000

    Initial Commit

---
+++ DebugImageCreator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DebugImageCreator
+//
+//  Created by Andy Qua on 02/03/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
